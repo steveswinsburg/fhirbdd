81936cda2c9af7bb49e35f2c799700fcbdd6b6b3
Steve Swinsburg <steve.swinsburg@gmail.com>
2025-05-07 11:12:12 +1000

    Update CLI command

--- src/cli.ts
+++ src/cli.ts
@@ -5,8 +5,8 @@ import { run } from "./index";
 const program = new Command();
 
 program
-  .name("firebed")
-  .description("Generate BDD tests from FHIR Implementation Guides")
+  .name("firebdd")
+  .description("Generate BDD feature files from FHIR Implementation Guides")
   .version("0.1.0")
   .option("--ig <path>", "Path to FHIR IG package (.tgz file)")
   .option("--output <dir>", "Output directory for feature files")
